bf9bd2678ac062aa69b8cbd67c59406324d5987c
big speedy <aditocco2@gmail.com>
2026-02-23 22:44:27 -0500

    1000 HZ HUB75 DRIVER IN PROGRESS
    disclaimer: it's 1000 Hz on low brightness. refresh rate depends on the pulse width.

--- drivers/hub75_v2.c
+++ drivers/hub75_v2.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
+#include "hardware/dma.h"
 #include "images/test_gradient.h"
 #include "hub75_v2.pio.h"
 
@@ -22,8 +23,8 @@
 
 static PIO pio = pio0;
 // locations in memory of PIO programs
-static uint32_t data_program_offset;
-static uint32_t row_program_offset;
+static uint32_t data_prog_offset;
+static uint32_t row_prog_offset;
 
 // The back buffer in our case right now is the test gradient from the header file
 static uint16_t *back_buffer;
@@ -32,37 +33,63 @@ static uint32_t front_buffer[WIDTH * HEIGHT];
 
 static uint32_t bytes_per_bitplane = WIDTH * HEIGHT/2;
 
-// 
+static uint32_t refresh_count;
+static absolute_time_t ts;
+
 static uint32_t pulse_width_buffer[] = {
-    100, 200, 400, 800, 1600, 3200, 6400, 12800
+   10, 20, 40, 80, 160, 320, 640, 1280
 };
 
 void hub75_configure();
 void hub75_load_image();
 void hub75_push();
-void hub75_refresh();
+void hub75_begin();
 void make_bitplanes(uint16_t *in_565, uint8_t *out_888);
 inline uint32_t rgb565_to_rgb888(uint16_t pix);
+void dma_complete();
 
 void main(void){
     stdio_init_all();
     hub75_configure();
     hub75_load_image(test_gradient);
     hub75_push();
+    hub75_begin();
     while(1){
-        hub75_refresh();
-        printf("refresh finished\n");
+        // print refresh count every second
+        if (absolute_time_diff_us(ts, get_absolute_time()) > 1000000){
+            ts = get_absolute_time();
+            printf("Refresh rate: %d Hz\n", refresh_count);
+            refresh_count = 0;
+        }
     }
 }
 
+int rgb_dma_channel, pw_dma_channel;
+dma_channel_config rgb_dma_config, pw_dma_config;
+
 void hub75_configure(){
     // Get the locations (offsets) of the PIO programs in memory
-    data_program_offset = pio_add_program(pio, &hub75_data_program);
-    row_program_offset = pio_add_program(pio, &hub75_row_program);
+    data_prog_offset = pio_add_program(pio, &hub75_data_program);
+    row_prog_offset = pio_add_program(pio, &hub75_row_program);
 
     // initialize PIO programs, passing in screen width to the data program
-    hub75_data_program_init(pio, DATA_SM, data_program_offset, DATA_BASE_PIN, CLK_PIN, WIDTH);
-    hub75_row_program_init(pio, ROW_SM, row_program_offset, ROWSEL_BASE_PIN, ROWSEL_N_PINS, STROBE_PIN);
+    hub75_data_program_init(pio, DATA_SM, data_prog_offset, DATA_BASE_PIN, CLK_PIN, WIDTH);
+    hub75_row_program_init(pio, ROW_SM, row_prog_offset, ROWSEL_BASE_PIN, ROWSEL_N_PINS, STROBE_PIN);
+
+    // initial setup of both DMA channels
+    rgb_dma_channel = dma_claim_unused_channel(true);
+    rgb_dma_config = dma_channel_get_default_config(rgb_dma_channel);
+    channel_config_set_transfer_data_size(&rgb_dma_config, DMA_SIZE_32);
+    channel_config_set_read_increment(&rgb_dma_config, true);
+    channel_config_set_write_increment(&rgb_dma_config, false);
+    channel_config_set_dreq(&rgb_dma_config, pio_get_dreq(pio, DATA_SM, true));
+
+    pw_dma_channel = dma_claim_unused_channel(true);
+    pw_dma_config = dma_channel_get_default_config(pw_dma_channel);
+    channel_config_set_transfer_data_size(&pw_dma_config, DMA_SIZE_32);
+    channel_config_set_read_increment(&pw_dma_config, true);
+    channel_config_set_write_increment(&pw_dma_config, false);
+    channel_config_set_dreq(&pw_dma_config, pio_get_dreq(pio, ROW_SM, true));
 }
 
 void hub75_load_image(uint16_t * image_pointer){
@@ -74,18 +101,40 @@ void hub75_push(){
     make_bitplanes(back_buffer, (uint8_t *)front_buffer);
 }
 
-void hub75_refresh(){
+void hub75_begin(){
+    // Make RGB DMA trigger an IRQ on completion
+    dma_channel_set_irq0_enabled(rgb_dma_channel, true);
+    irq_set_exclusive_handler(DMA_IRQ_0, dma_complete);
+    irq_set_enabled(DMA_IRQ_0, true); 
 
-    int i = 0;
+    // Make RGB DMA copy from front buffer to Data SM FIFO
+    dma_channel_configure(
+        rgb_dma_channel, &rgb_dma_config,
+        &pio->txf[DATA_SM], front_buffer,
+        WIDTH * HEIGHT, false
+    );
 
-    for(uint8_t bit = 0; bit < 8; bit++){
-        pio_sm_put_blocking(pio, ROW_SM, pulse_width_buffer[bit]);
-    }
-    for(uint32_t data_index = 0; data_index < WIDTH * HEIGHT; data_index++){
-        pio_sm_put_blocking(pio, DATA_SM, front_buffer[i]);
-        i++;
-    }
+    // Make PW DMA copy from pulse buffer to Row SM FIFO
+    dma_channel_configure(
+        pw_dma_channel, &pw_dma_config,
+        &pio->txf[ROW_SM], pulse_width_buffer,
+        8, false // 8 pulse widths for 8 bits
+    );
 
+    // Start both channels
+    dma_channel_start(pw_dma_channel);
+    dma_channel_start(rgb_dma_channel);
+}
+
+void dma_complete(){
+    // CLEAR THE INTERRUPT
+    dma_irqn_acknowledge_channel(0, rgb_dma_channel);
+
+    // reconfigure both DMA channels
+    dma_hw->ch[rgb_dma_channel].al3_read_addr_trig = (uint32_t)front_buffer;
+    dma_hw->ch[pw_dma_channel].al3_read_addr_trig = (uint32_t)pulse_width_buffer;
+
+    refresh_count++;
 }
 
 void make_bitplanes(uint16_t *in_565, uint8_t *out_888){
@@ -116,8 +165,17 @@ void make_bitplanes(uint16_t *in_565, uint8_t *out_888){
 inline uint32_t rgb565_to_rgb888(uint16_t pix) {
 
     // 00000000 00000000 RRRRRGGG GGGBBBBB -> 00000000 BBBBB000 GGGGGG00 RRRRR000
-    uint8_t r = ((pix & 0xF800) >> 11 << 3);
-    uint8_t g = ((pix & 0x07E0) >> 5 << 2);
-    uint8_t b = ((pix & 0x001F) >> 0 << 3);
-    return (b << 16) | (g << 8) | (r << 0);
+    // uint8_t r = ((pix & 0xF800) >> 11 << 3);
+    // uint8_t g = ((pix & 0x07E0) >> 5 << 2);
+    // uint8_t b = ((pix & 0x001F) >> 0 << 3);
+    // return (b << 16) | (g << 8) | (r << 0);
+
+    // gamma correct!!!
+    uint32_t r_gamma = pix & 0xf800u;
+    r_gamma *= r_gamma;
+    uint32_t g_gamma = pix & 0x07e0u;
+    g_gamma *= g_gamma;
+    uint32_t b_gamma = pix & 0x001fu;
+    b_gamma *= b_gamma;
+    return (b_gamma >> 2 << 16) | (g_gamma >> 14 << 8) | (r_gamma >> 24 << 0);
 }
